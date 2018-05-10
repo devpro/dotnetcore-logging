781ff57812d9d2e2fd066b3115c46dbe27f35a21
Bertrand THOMAS <bertrand@devpro.fr>
2018-05-10 15:33:48 +0200

    Rename namespace with fully qualified name

--- src/WebAppWithSerilog/Controllers/ValuesController.cs
+++ src/WebAppWithSerilog/Controllers/ValuesController.cs
@@ -1,10 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using System.Collections.Generic;
 using Microsoft.AspNetCore.Mvc;
 
-namespace WebAppWithSerilog.Controllers
+namespace Devpro.DotNetCoreLogging.WebAppWithSerilog.Controllers
 {
     [Route("api/[controller]")]
     public class ValuesController : Controller
